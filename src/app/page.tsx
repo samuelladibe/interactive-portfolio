70c655e346497f2247bacd9f9da83e30e30a396f
samuelladibe <ladibesamuel@gmail.com>
2025-12-16 19:24:10 +0100

    update: add SkillsSection to the import and the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 // src/app/page.tsx
 import HeroSection from '@/components/HeroSection';
-import ExperienceSection from '@/components/ExperienceSection'; // Import the new section
+import ExperienceSection from '@/components/ExperienceSection';
+import SkillsSection from '@/components/SkillsSection'; // Import the new section
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <HeroSection />
       <ExperienceSection />
-      {/* Other sections will go here */}
+      <SkillsSection /> {/* Add the Skills section here */}
+      {/* Contact and other sections will go here */}
     </main>
   );
 }
